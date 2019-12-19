5cc197e55af069bc6ae047fa2cfd6ecb2639575e
Peteris Krumins <peteris.krumins@gmail.com>
2019-12-19 17:38:34 +0000

    bin: add i3-show-or-hide-calendar.sh

---
+++ bin/i3-show-or-hide-calendar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+
+if xdotool search -classname "calendar" &>/dev/null; then
+    i3-msg '[instance="calendar"] kill'
+else
+    urxvt -name calendar -fn "xft:DejaVu Sans Mono:size=9" -e bash -i -c "ncal -w -b -B 5 -A 6 -M && read -r -s -n 1"
+fi
+
