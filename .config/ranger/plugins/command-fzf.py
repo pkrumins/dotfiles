ec54478bc2f49426553071828b39efce64b93507
Peteris Krumins <peteris.krumins@gmail.com>
2020-03-13 09:27:18 +0000

    ranger: add fzf plugin

---
+++ .config/ranger/plugins/command-fzf.py
@@ -0,0 +1,28 @@
+from ranger.api.commands import Command
+import subprocess
+import os.path
+
+class fzf_select_file(Command):
+    def execute(self):
+        command="rg --files --no-ignore --hidden -g '!.git' | fzf"
+        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+
+class fzf_select_dir(Command):
+    def execute(self):
+        command="fd --type d --no-ignore -H -E '.git' | fzf"
+        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+
