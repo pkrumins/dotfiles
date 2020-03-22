04cf4f5e0311cf93876da3008d9a7afe92914a28
Peteris Krumins <peteris.krumins@gmail.com>
2020-03-22 18:22:29 +0000

    bin: ws2: attach to ws2 if ws2 exists, else create ws2

--- bin/ws2.sh
+++ bin/ws2.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 #
 
-tmux new-session -s 'ws2' -d
-tmux new-window -n 'w1'       "dmesg -T && free && uptime && /bin/bash -i"
-tmux new-window -n 'tunnels'
-tmux new-window -n 'tools'    -c ~/projects/onlinetools
-tmux new-window -n 'catonmat' -c ~/projects/catonmat-new
-tmux new-window -n 'br-web'   -c ~/projects/browserling-web
-tmux new-window -n 'br-new'   -c ~/projects/browserling-new
-tmux new-window -n 'br-comic' -c ~/projects/browserling-comic
-tmux new-window -n 'urls'     -c ~/projects/techurls
-tmux new-window -n 'gmsg'     -c ~/projects/browserling-gmsg
-tmux new-window -n 't'        -c ~/projects
-tmux new-window -n 'r'        -c ~/projects
+if { tmux list-sessions | grep -q ^ws2; } 1>/dev/null 2>&1; then
+    tmux attach-session -d -t "ws2"
+else
+    tmux new-session -s 'ws2' -n "0" -d
 
-tmux select-window -t "w1"
+    tmux new-window -n 'tun'
+    tmux new-window -n 'tools'    -c ~/projects/onlinetools
+    tmux new-window -n 'catonmat' -c ~/projects/catonmat-new
+    tmux new-window -n 'br-web'   -c ~/projects/browserling-web
+    tmux new-window -n 'br-new'   -c ~/projects/browserling-new
+    tmux new-window -n 'br-comic' -c ~/projects/browserling-comic
+    tmux new-window -n 'urls'     -c ~/projects/techurls
 
-tmux attach-session -d
+    tmux select-window -t "0"
+
+    tmux attach-session -d
+fi
